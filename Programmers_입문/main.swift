ca86f90cd0a58afdc03bd46b490b178b14dc7758
이대호 <dooho199234@nate.com>
2023-12-19 13:10:48 +0900

    폴더명 변경

---
+++ Programmers_입문/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 이대호 on 2023/12/19.
+//  프로그래머스 - 입문
+//  점위 위치 구하기(https://school.programmers.co.kr/learn/courses/30/lessons/120841)
+
+import Foundation
+
+func solution(_ dot:[Int]) -> Int {
+    return 0
+}
