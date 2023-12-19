be0593827cfdcaa18dd633daef02cfc0887beae4
이대호 <dooho199234@nate.com>
2023-12-19 13:10:48 +0900

    폴더명 변경

--- Programmers/main.swift
+++ Programmers/main.swift
@@ -9,5 +9,20 @@
 import Foundation
 
 func solution(_ dot:[Int]) -> Int {
-    return 0
+    var result = 0
+    
+    if dot[0] > 0 && dot[1] > 0 {
+       result = 1
+    }else if dot[0] < 0 && dot[1] > 0 {
+        result = 2
+    }else if dot[0] < 0 && dot[1] < 0 {
+        result = 3
+    }else {
+        result = 4
+    }
+    
+    return result
 }
+
+
+print(solution([-3,2]))
