a4654393dcc1147b01da667a9fc7f7b89f4b98ca
Lee Daeho <83402908+leedaeho8078@users.noreply.github.com>
2023-12-20 11:48:37 +0900

    약수구하기

--- Programmers/약수구하기.swift
+++ Programmers/약수구하기.swift
@@ -1,1 +1,13 @@
 
+//  약수 구하기.swift
+//  Created by 이대호 on 2023/12/20.
+//  프로그래머스 - 입문
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120897
+
+
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    return (1...n).filter{ n % $0 == 0}
+}
+
