82aa336008917374b09fbe26da237d16a572c43b
Lee Daeho <83402908+leedaeho8078@users.noreply.github.com>
2023-12-20 11:15:17 +0900

    12_20_문자열 정렬하기(1).swift

--- Programmers/문자열 정렬하기(1).swift
+++ Programmers/문자열 정렬하기(1).swift
@@ -1,14 +1,14 @@
 
-- 문자열 정렬하기 (1) .swift
-- Created by 이대호 on 2023/12/30.
-- 프로그래머스 - 입문
-- https://school.programmers.co.kr/learn/courses/30/lessons/120850 
+// 문자열 정렬하기 (1) .swift
+// Created by 이대호 on 2023/12/30.
+// 프로그래머스 - 입문
+// https://school.programmers.co.kr/learn/courses/30/lessons/120850 
 
-```Swift
+
 import Foundation
 
 func solution(_ my_string:String) -> [Int] {
     return my_string.filter { $0.isNumber }.map { Int(String($0))!}.sorted(by: <)
 }
 
-```
+
